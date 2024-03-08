65c1fc73672b0d953917ee2c9a70639c48ef4d3d
Moknine Elhadj <mokninemoknine999@gmail.com>
2024-03-08 20:00:06 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/shared/Header"
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="">
+      <Header/>
+      <Button>Add</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,21 @@
+
+const Header = () => {
+  return (
+    <header className="px-2 py-4 border-b">
+        <div className="container mx-auto flex justify-between items-center">
+            <p>Logo</p>
+            <nav className="space-x-4 flex">
+                <p>About</p>
+                <p>Products</p>
+                <p>Store</p>
+            </nav>
+        </div>
+
+
+
+
+    </header>
+  )
+}
+
+export default Header
